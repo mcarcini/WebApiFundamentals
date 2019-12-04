364621dffcfc9f4ffbf9081f597e1639d193c113
Marco Carcini <marco.carcini@hotmail.com>
2019-12-03 21:49:48 -0600

    add method to get an individual item

--- starting/TheCodeCamp/App_Start/WebApiConfig.cs
+++ starting/TheCodeCamp/App_Start/WebApiConfig.cs
@@ -19,11 +19,13 @@ namespace TheCodeCamp
       // Web API routes
       config.MapHttpAttributeRoutes();
 
+       /*
       config.Routes.MapHttpRoute(
           name: "DefaultApi",
           routeTemplate: "api/{controller}/{id}",
           defaults: new { id = RouteParameter.Optional }
       );
+      */
     }
   }
 }
--- starting/TheCodeCamp/Controllers/CampsController.cs
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -10,6 +10,7 @@ using TheCodeCamp.Models;
 
 namespace TheCodeCamp.Controllers
 {
+    [RoutePrefix("api/camps")]
     public class CampsController : ApiController
     {
         private readonly ICampRepository _repository;
@@ -19,7 +20,7 @@ namespace TheCodeCamp.Controllers
             _repository = repository;
             _mapper = mapper;
         }
-
+        [Route()]
         public async Task<IHttpActionResult> Get()
         {
             try
@@ -27,12 +28,25 @@ namespace TheCodeCamp.Controllers
                 var result = await _repository.GetAllCampsAsync();
                 var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
 
+                //return NotFound();
                 return Ok(mappedResult);
             }
             catch (Exception e) {
                 return InternalServerError(e);
+            }            
+        }
+
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Get(String moniker) {
+            try {
+                var result = await _repository.GetCampAsync(moniker);
+                if (result == null) return NotFound();
+
+                return Ok(_mapper.Map<CampModel>(result));
             }
-            
+            catch (Exception e) {
+                return InternalServerError(e);
+            }
         }
     }
 }
